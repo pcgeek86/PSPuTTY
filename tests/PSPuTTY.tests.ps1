a66916160438c4315800c3583ff178d1c632028f
Trevor Sullivan <trevor.sullivan@clusterhq.com>
2016-10-28 17:22:12 -0600

    Updated Pester tests for PSPuTTY

--- tests/PSPuTTY.tests.ps1
+++ tests/PSPuTTY.tests.ps1
@@ -1,29 +1,53 @@
+<#
+Assuming that this module is in your $env:PSModulePath, you can invoke this Pester test script:
+
+  Set-Location -Path ('{0}\Tests' -f (Get-Module -Name PSPuTTY -ListAvailable).ModuleBase);
+  ./PSPuTTY.tests.ps1
+#>
+
+$VerbosePreference = 'continue'
+$ErrorActionPreference = 'stop'
+
 describe -Name 'PSPuTTY PowerShell module' {
     $ModuleName = 'PSPuTTY'
-    $ModulePath = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+    $ModulePath = Split-Path -Path $PSScriptRoot -Parent
     if (Get-Module -Name $ModuleName) {
         Remove-Module -Name $ModuleName
     }
-    Import-Module -Name $ModuleName -Force
+    Import-Module -Name $ModulePath -Force
+    Write-Verbose -Message ('Module path is: {0}' -f $ModulePath)
 
-    it 'Should have a Get-PSPuTTYSession command' {
-        (Get-Command -Module $ModuleName -Name Get-PSPuTTYSession).Count | Should be 1
+    context -Name 'Basic presence of core commands' {
+        it 'Should have a Get-PSPuTTYSession command' {
+            (Get-Command -Module $ModuleName -Name Get-PSPuTTYSession).Count | Should be 1
+        }
+
+        it 'Should have a Get-PSPuTTYTheme command' {
+            (Get-Command -Module $ModuleName -Name Get-PSPuTTYTheme).Count | Should be 1
+        }
+
+        it 'Should have a Merge-PSPuTTYTheme command' {
+            (Get-Command -Module $ModuleName -Name Merge-PSPuTTYTheme).Count | Should be 1
+        }
+
+        it 'Should have a Invoke-PSPuTTYSession command' {
+            (Get-Command -Module $ModuleName -Name Invoke-PSPuTTYSession).Count | Should be 1
+        }
+
+        it 'Should have a New-PSPuTTYSession command' {
+            (Get-Command -Module $ModuleName -Name New-PSPuTTYSession).Count | Should be 1
+        }
     }
 
-    it 'Should have a Get-PSPuTTYTheme command' {
-        (Get-Command -Module $ModuleName -Name Get-PSPuTTYTheme).Count | Should be 1
+    context -Name 'Get-PSPuTTYTheme' {
+        it 'Should not throw an error' {
+            { Get-PSPuTTYTheme } | Should not throw
+        }
     }
 
-    it 'Should have a Merge-PSPuTTYTheme command' {
-        (Get-Command -Module $ModuleName -Name Merge-PSPuTTYTheme).Count | Should be 1
+    context -Name 'Get-PSPuTTYSession' {
+        it 'Should not throw an error' {
+            { Get-PSPuTTYSession } | Should not throw
+        }
     }
-
-    it 'Should have a Invoke-PSPuTTYSession command' {
-        (Get-Command -Module $ModuleName -Name Invoke-PSPuTTYSession).Count | Should be 1
-    }
-
-    it 'Should have a New-PSPuTTYSession command' {
-        (Get-Command -Module $ModuleName -Name New-PSPuTTYSession).Count | Should be 1
-    }
-
 }
