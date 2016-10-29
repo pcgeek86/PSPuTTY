8b4307f39f98ac456f0e6770617f2bd8b165be15
Trevor Sullivan <Trevor Sullivan>
2016-10-29 17:24:17 -0500

    Implemented the Merge-PSPuTTYTheme command

--- Functions/Public/Merge-PSPuTTYTheme.ps1
+++ Functions/Public/Merge-PSPuTTYTheme.ps1
@@ -1,3 +1,5 @@
+using namespace System.Web
+
 function Merge-PSPuTTYTheme {
     <#
     .Synopsis
@@ -5,6 +7,9 @@ function Merge-PSPuTTYTheme {
 
     .Parameter SessionName
     The name of the PuTTY saved session that will have its theme updated.
+    
+    NOTE: The session name will be URL-encoded, to conform to the stored value in the registry. Make sure that you do not use the URL-encoded name of the session.
+          Rather, use the "friendly name" of the session, that appears in the PuTTY Saved Session list.
 
     .Parameter ThemeName
     The name of the PuTTY theme that will be configured on the saved session.
@@ -14,13 +19,43 @@ function Merge-PSPuTTYTheme {
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, ParameterSetName = 'SessionName')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'SessionNameThemeName')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'SessionNameThemeObject')]
         [string] $SessionName
-      , [Parameter(Mandatory = $true, ParameterSetName = 'SessionObject')]
+      , [Parameter(Mandatory = $true, ParameterSetName = 'SessionObjectThemeName')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'SessionObjectThemeObject')]
         [PSPuTTYSession] $Session
-      , [Parameter(Mandatory = $true)]
+      , [Parameter(Mandatory = $true, ParameterSetName = 'SessionObjectThemeName')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'SessionNameThemeName')]
         [string] $ThemeName
+      , [Parameter(Mandatory = $true, ParameterSetName = 'SessionNameThemeObject')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'SessionObjectThemeObject')]
+        [PSPuTTYTheme] $Theme
     )
 
-    
+    Write-Verbose -Message
+    Write-Verbose -Message ('Using parameter set: {0}' -f $PSCmdlet.ParameterSetName)
+
+    ### If the user specifies a session name, then we need to URL encode it
+    if ($PSCmdlet.ParameterSetName -match 'SessionName') {
+      Write-Verbose -Message 'Caller specified session name. Obtaining session object.'
+      $Session = Get-PSPuTTYSession -Name $SessionName
+    }
+
+    if ($PSCmdlet.ParameterSetName -match 'ThemeName') {
+      Write-Verbose -Message 'Caller specified theme name. Obtaining theme object.'
+      $Theme = Get-PSPuTTYTheme -Name $ThemeName
+    }
+
+    Write-Verbose -Message ($Session)
+    Write-Verbose -Message ('Opening PuTTY session subkey: {0}' -f $Session.Name)
+    $PuTTYSessionReg = (Get-PSPuTTYRegistrySessions).OpenSubkey($Session.Name, $true)
+
+    $SessionKey = $PuTTYSessionReg.OpenSubkey($Session.Name)
+    foreach ($Number in 0..21) {
+      Write-Verbose -Message ('Setting Colour{0} to {1}' -f $Number, $Theme."Colour$Number")
+      $PuTTYSessionReg.SetValue("Colour$Number", $Theme."Colour$Number", [Microsoft.Win32.RegistryValueKind]::String)
+    }
+
+    $PuTTYSessionReg.SetValue('TerminalType', 'putty-256color')
 }
