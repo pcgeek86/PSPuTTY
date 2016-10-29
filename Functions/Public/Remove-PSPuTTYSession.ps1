fade9216697cbd93f81554e8d1a8cce772508e26
Trevor Sullivan <trevor.sullivan@clusterhq.com>
2016-10-28 18:11:26 -0600

    Adding Remove-PSPuTTYSession function

---
+++ Functions/Public/Remove-PSPuTTYSession.ps1
@@ -0,0 +1,13 @@
+function Remove-PSPuTTYSession {
+  <#
+  .Synopsis
+  Removes a PuTTY saved session from the user's profile.
+  #>
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $Name
+  )
+
+  ### TODO: Implement this function
+}
